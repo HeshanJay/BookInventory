3de611d55a1875724278354f1e6b7c9a721c9473
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2026-02-19 13:40:49 +0530

    Implement in-memory BookService

--- BookInventory.Api/Services/BookService.cs
+++ BookInventory.Api/Services/BookService.cs
@@ -1,6 +1,40 @@
-﻿namespace BookInventory.Api.Services
+﻿using BookInventory.Api.Models;
+
+namespace BookInventory.Api.Services
 {
-    public class BookService
+    public static class BookService
     {
+        private static List<Book> Books { get; } = new List<Book>();
+        private static int _nextId = 1;
+
+        public static List<Book> GetAll() => Books;
+
+        public static Book? Get(int id) =>
+            Books.FirstOrDefault(b => b.Id == id);
+
+        public static Book Add(Book book)
+        {
+            book.Id = _nextId++;
+            Books.Add(book);
+            return book;
+        }
+
+        public static bool Update(Book book)
+        {
+            var index = Books.FindIndex(b => b.Id == book.Id);
+            if (index == -1) return false;
+
+            Books[index] = book;
+            return true;
+        }
+
+        public static bool Delete(int id)
+        {
+            var book = Get(id);
+            if (book is null) return false;
+
+            Books.Remove(book);
+            return true;
+        }
     }
 }
