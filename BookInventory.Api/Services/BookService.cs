9c23980fabdf4217ee08b76628578ad97d6e5fd0
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2026-02-20 06:41:27 +0530

    Add default book data to BookService

--- BookInventory.Api/Services/BookService.cs
+++ BookInventory.Api/Services/BookService.cs
@@ -7,6 +7,19 @@ namespace BookInventory.Api.Services
         private static List<Book> Books { get; } = new List<Book>();
         private static int _nextId = 1;
 
+        static BookService()
+        {
+            Books = new List<Book>
+            {
+                new Book { Id = 1, Title="Clean Code", Author="Robert C. Martin", Isbn="9780132350884", PublicationDate = new DateTime(2008,8,1) },
+                new Book { Id = 2, Title="Atomic Habits", Author="James Clear", Isbn="9780735211292", PublicationDate = new DateTime(2018,10,16) },
+                new Book { Id = 3, Title="Pragmatic Programmer", Author="Andrew Hunt", Isbn="9780201616224", PublicationDate = new DateTime(1999,10,30) },
+                new Book { Id = 4, Title="Design Patterns", Author="Erich Gamma", Isbn="9780201633610", PublicationDate = new DateTime(1994,10,31) },
+            };
+            _nextId = 5;
+        }
+
+
         public static List<Book> GetAll() => Books;
 
         public static Book? Get(int id) =>
